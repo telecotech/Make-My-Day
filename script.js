be7fbd65d8e5479ac220f83be28066b106c7a82a
Mullens <telecotech82@gmail.com>
2023-02-25 15:48:52 -0500

    adding style and getting things working

--- script.js
+++ script.js
@@ -1,74 +1,33 @@
-// // Define the displayTime() function to display the current date and time in the header
-// var now = dayjs();
-// var formattedDateTime = now.format("dddd, MMMM D, YYYY h:mm A");
-// $("#currentDay").text(formattedDateTime);
-
-
-// $(".saveBtn").on("click", function() {
-//   // Get the id of the time block
-//   var timeBlockId = $(this).parent().attr("id");
-
-//   // Get the text entered in the text area
-//   var text = $(this).siblings(".description").val().trim();
-
-//   // Save the text to local storage using the time block id as the key
-//   localStorage.setItem(timeBlockId, text);
-//   console.log('function working')
-// });
-
-
-
-// // Load the saved data for each textarea
-// $('.time-block').each(function() {
-//   // Get the ID of the parent element
-//   var id = $(this).attr('id');
-  
-//   // Get the saved value from local storage
-//   var value = localStorage.getItem(id);
-  
-//   // If there is saved data, set the value of the textarea
-//   if (value !== null) {
-//     $(this).find('textarea').val(value);
-//   }
-// });
-
-
-
-
-
-
-
-// trying something
-
-
 $(document).ready(function() {
 
   // Define the displayTime() function to display the current date and time in the header
   var now = dayjs();
-  var formattedDateTime = now.format("dddd, MMMM D, YYYY h:mm A");
+  var formattedDateTime = now.format("dddd, MMMM D, YYYY H:mm A");
   $("#currentDay").text(formattedDateTime);
 
-  // Function to update text area color based on current time
-  function updateTextAreaColor() {
-    var currentHour = dayjs().hour();
-    $('.time-block').each(function() {
-      var blockHour = parseInt($(this).attr('id'));
-      var textArea = $(this).find('textarea');
-      if (blockHour < currentHour) {
-        textArea.removeClass('present future').addClass('past');
-      } else if (blockHour === currentHour) {
-        textArea.removeClass('past future').addClass('present');
-      } else {
-        textArea.removeClass('past present').addClass('future');
-      }
-    });
-  }
+ 
 
-  // Call the updateTextAreaColor() function every 5 minutes to keep the colors updated
-  setInterval(updateTextAreaColor, 5 * 60 * 1000);
+  $('.time-block').each(function() {
+    var hour = ($(this).attr('id'));
+    
+    if (hour < now.hour()) {
+      // past hour
+      $(this).addClass("past").removeClass("present future");
+    } else if (hour == now.hour()) {
+      // current hour
+      $(this).addClass("present").removeClass("past future");
+    } else {
+      // future hour
+      $(this).addClass("future").removeClass("past present");
+    }
+    console.log(hour)
+  });
+  
+  
+ 
+  
 
-  // Call the updateTextAreaColor() function once on page load
-  updateTextAreaColor();
+ 
 
   $(".saveBtn").on("click", function() {
     // Get the id of the time block
@@ -96,7 +55,6 @@ $(document).ready(function() {
   });
 
 });
-
 
 
 
